f17e6fcf5cea065dfcf0e080e44143c9b32fc8bf
Konrad <konradR1999@gmail.com>
2019-05-17 11:32:44 +0200

    Dodanie poziomÃw trudnosci oraz przycisku wyjscia z gry po jej zakonczeniu.

---
+++ Memorygame2/src/plan/SceneLib.java
@@ -0,0 +1,56 @@
+package plan;
+
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import plan.SceneOne;
+
+public abstract class SceneLib {
+    private static Stage primary;
+    private static Scene sceneOne = new Scene(new SceneOne().makeParent());
+    private static Scene sceneTwo = new Scene(new SceneTwo(), 500, 500);
+    private static Scene sceneThree = new Scene(new SceneThree(), 500, 500);
+
+    public static void setPrimary(Stage primary) {
+        SceneLib.primary = primary;
+    }
+
+    public static void switchtoOne() {
+        sceneOne = new Scene(new SceneOne().makeParent());
+        primary.setScene(sceneOne);
+    }
+
+    public static void switchtoTwo() {
+        primary.setScene(sceneTwo);
+    }
+    
+       public static void switchtoThree() {
+        primary.setScene(sceneThree);
+    }
+    
+    public static void end() {
+        primary.close();
+    }
+    
+    public static void setNumberOfPairs(int x){ // ???????????
+        SceneOne.numberOfPairs = x;
+        
+    }
+    
+     public static void easy() {
+        SceneLib.setNumberOfPairs(5);
+        SceneLib.switchtoOne();
+    }
+
+    public static void medium() {
+
+        SceneLib.setNumberOfPairs(10);
+        SceneLib.switchtoOne();
+    }
+
+    public static void hard() {
+        SceneLib.setNumberOfPairs(15);
+        SceneLib.switchtoOne();
+    }
+
+
+}
