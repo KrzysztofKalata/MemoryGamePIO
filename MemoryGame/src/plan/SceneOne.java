a2383a11fe4b7769df938cd70bf97c5a345023ac
Konrad <konradR1999@gmail.com>
2019-05-20 13:14:50 +0200

    ostateczna wersja

--- MemoryGame/src/plan/SceneOne.java
+++ MemoryGame/src/plan/SceneOne.java
@@ -19,34 +19,31 @@ import javafx.scene.paint.Color;
 import javafx.scene.paint.ImagePattern;
 
 public class SceneOne {
+
     Pane ley = new Pane();
     public static int numberOfPairs = 3;
     private final double dimension = Math.ceil(Math.sqrt(numberOfPairs * 2));
     private card isChosen = null;
     private int points = 0;
-    public int id=0;
+    public int id = 0;
 
-    
     public Pane makeParent() {
-        
-        
+
         ley.setPrefSize(70 * dimension, 70 * dimension);
-        ArrayList<card> al = new ArrayList<>();
+        ArrayList<card> cardList = new ArrayList<>();
         int i;
-      
-       for (i = 0; i < numberOfPairs; i++) {
-           int idCard=i;
-           
-           
-            al.add(new card(new Image("img/"+idCard+".jpg"),i));
-            
-            al.add(new card(new Image("img/"+idCard+".jpg"),i));
+
+        for (i = 0; i < numberOfPairs; i++) {
+            int idCard = i;
+
+            cardList.add(new card(new Image("img/" + idCard + ".jpg"), i));
+
+            cardList.add(new card(new Image("img/" + idCard + ".jpg"), i));
             int image = 1;
 
         }
-      
-       
-        Collections.shuffle(al);
+
+        Collections.shuffle(cardList);
 
         double row = 0;
         double column = 0;
@@ -57,46 +54,40 @@ public class SceneOne {
                 column = 0;
                 row++;
             }
-            al.get(b).location(column, row);
-            ley.getChildren().add(al.get(b));
+            cardList.get(b).location(column, row);
+            ley.getChildren().add(cardList.get(b));
             column++;
 
         }
         return ley;
     }
 
-   
-
-       
-   
-
     public final class card extends StackPane {
 
         private Text sign = new Text();
-    //    private RectangleShape kasia=new RectangleShape();
+        //    private RectangleShape kasia=new RectangleShape();
         private Rectangle border = new Rectangle();
-        public int id=3;
-         ImagePattern image;
-       private Rectangle lol = new Rectangle();
-        
-       public int getId(int i){
-         return i;  
-       }
-        public card(Image image,int id) {
-            
-            
-    lol= new Rectangle(60,60);
+        public int id = 3;
+        ImagePattern image;
+        private Rectangle lol = new Rectangle();
+
+        public int getId(int i) {
+            return i;
+        }
+
+        public card(Image image, int id) {
+
+            lol = new Rectangle(60, 60);
             border = new Rectangle(50, 50);
             lol.setFill(Paint.valueOf("#DDDDDD"));
             lol.setStroke(Paint.valueOf("#000000"));
             border.setFill(Paint.valueOf("#DDDDDD"));
             border.setFill(new ImagePattern(image, 0, 0, 1, 1, true));
             border.setStroke(Paint.valueOf("#000000"));
-            
-         sign.setId(id+"");
-         
-          
-            getChildren().addAll(lol,border, sign);
+
+            sign.setId(id + "");
+
+            getChildren().addAll(lol, border, sign);
             close();
             setPadding(new Insets(5, 5, 5, 5));
             setOnMouseClicked(event -> {
@@ -107,7 +98,7 @@ public class SceneOne {
                     cardClick();
                     if (checkPair(this, isChosen) == true && this != isChosen) {
                         ley.getChildren().removeAll(this, isChosen);
-                        
+
                         isChosen = null;
                         points++;
                         if (points == numberOfPairs) {
@@ -126,7 +117,6 @@ public class SceneOne {
             });
 
         }
-        
 
         public boolean checkPair(card first, card second) {
             if (first.sign.getId().equals(second.sign.getId())) {
@@ -135,14 +125,13 @@ public class SceneOne {
                 return false;
             }
         }
-        
 
         public void location(double column, double row) {
             setTranslateX(70 * column);
             setTranslateY(70 * row);
         }
 
-       public void cardClick() {
+        public void cardClick() {
             FadeTransition fade = new FadeTransition(Duration.seconds(0.5), border);
             fade.setToValue(1);
             fade.play();
@@ -156,9 +145,5 @@ public class SceneOne {
             ft.play();
         }
 
-        
-
-        
     }
 }
-
